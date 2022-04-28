4a00cd8f247846735373c852ff50302fbbb249b7
Tash Musanhu <tashinga.musanhu@and.digital>
2022-04-28 23:08:11 +0100

    feat: :lipstick: button to add tasks

--- to-do-app/src/Components/MainContainer.jsx
+++ to-do-app/src/Components/MainContainer.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Tasks from "./Tasks";
 import "../scss/Container.scss";
+import "../scss/Buttons.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faPlus} from "@fortawesome/free-solid-svg-icons";
 
 export default function MainContainer() {
+	const icon = <FontAwesomeIcon icon={faPlus} color={"white"} />
 	return (
 		<div className="container-main">
 			<h1 className="container-title">Tasks</h1>
-				<Tasks/>
+			<Tasks />
+			<div className="test-div">
+				<button className="button-circle">{icon}</button>
+			</div>
 		</div>
 	);
 }
--- to-do-app/src/Components/Menu.jsx
+++ to-do-app/src/Components/Menu.jsx
@@ -9,8 +9,7 @@ import { faSquare } from "@fortawesome/free-regular-svg-icons";
 export default function Menu() {
 	return (
 		<div className="menu-div">
-			
-            <button className="menu-button"> <FontAwesomeIcon icon={faHouseChimney} color={'#3B7CF4'}/> </button>
+            <button name="woo" className="menu-button"> <FontAwesomeIcon icon={faHouseChimney} color={'#3B7CF4'}/> </button>
             <button className="menu-button"> <FontAwesomeIcon icon={faStar} color={'#FFD233'}/> </button>
             <button className="menu-button"> <FontAwesomeIcon icon={faSquareCheck} color={'#5CB85B'}/> </button>
             <button className="menu-button"> <FontAwesomeIcon icon={faSquare} color={'#848484'}/> </button>
